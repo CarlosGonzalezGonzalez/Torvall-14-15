086600e1b6d3e5949df8fe3e08cfb261d4dc8ebe
juangsp <juangsp@users.noreply.github.com>
2014-10-29 13:18:46 +0100

    update ejer02

--- repositorioEmpleado/src/es/torvall/GestorEmpleados.java
+++ repositorioEmpleado/src/es/torvall/GestorEmpleados.java
@@ -2,12 +2,49 @@ package es.torvall;
 
 public class GestorEmpleados {
  public static final String fichero = "./resources/empleados";
+ private static final ArrayList<Employee> employeeList=new ArrayList();
 
     public void cambiarSalario(int id, int salario) {
-        Employee e;
-        ArrayList<Employee> employeeList = new ArrayList();
-        ObjectInputStream streamEntrada = null;
         ObjectOutputStream streamSalida = null;
+        cargarFichero();
+        
+        for (Employee emp : employeeList) {
+            if (emp.getEmp_no() == id) {
+                emp.setSalary(salario);
+            }
+            System.out.println(emp.toString());
+        }
+        guardarFichero();
+    }
+
+    public void guardarFichero() {
+        ObjectOutputStream streamSalida=null;
+        
+        try {
+            streamSalida = new ObjectOutputStream(new FileOutputStream(fichero));
+
+            for (Employee emp : employeeList) {
+                streamSalida.writeObject(emp);
+            }
+
+            if (streamSalida == null) {
+                streamSalida.close();
+            }
+        } catch (IOException ex) {
+            System.err.println("Error E/S");
+        } finally {
+
+            try {
+                streamSalida.close();
+            } catch (IOException ex) {
+                System.err.println("Error E/S");
+            }
+        }
+    }
+
+    public void cargarFichero( ) {
+         Employee e= new Employee(); 
+        ObjectInputStream streamEntrada = null;
 
         try {
             /*El metodo lee del archivo que le hemos pasado
@@ -23,53 +60,21 @@ public class GestorEmpleados {
             }
 
         } catch (FileNotFoundException e1) {
-
+            System.err.println("Error, archivo no encontrado");
         } catch (EOFException e1) {
-
+            
         } catch (IOException e1) {
-
+            System.err.println("Error E/S");
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Ejercicio2.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Error clase no econtrada");
         } finally {
             try {
                 streamEntrada.close();
             } catch (IOException ex) {
-                Logger.getLogger(Ejercicio2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        /*Recorremos el arrayList comparando la id de cada uno de los empleados con
-         **la id que buscamos y cuando lo encontramos modificamos el valor de su salario
-         ** y le introducimos el salario que hemos recibido por parametro
-         */
-        for (Employee emp : employeeList) {
-            if (emp.getEmp_no() == id) {
-                emp.setSalary(salario);
-            }
-            System.out.println(emp.toString());
-        }
-        /*Por ultimo volvemos a escribir los cambios en el fichero
-        **
-        */
-        try {
-            streamSalida = new ObjectOutputStream(new FileOutputStream(fichero));
-
-            for (Employee emp : employeeList) {
-                streamSalida.writeObject(emp);
-            }
-
-            if (streamSalida == null) {
-                streamSalida.close();
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(Ejercicio2.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-
-            try {
-                streamSalida.close();
-            } catch (IOException ex) {
-                Logger.getLogger(Ejercicio2.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println("Error E/S");
             }
         }
     }
+
 
 }
